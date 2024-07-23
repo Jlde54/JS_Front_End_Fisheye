11e19f8daf087232f93765f6af5653c16f96a9cb
Jlde54 <jlde54@sfr.fr>
2024-07-23 14:28:43 +0200

    finalized Home page

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,28 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getPhotographers() {
+    // Récupération des photographes depuis le fichier JSON
+    const reponse = await fetch("../../data/photographers.json");
+    const photografers = await reponse.json();
+    return photografers;
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function displayData(photographers) {
+    // Affichage des portraits des photographes sur la page d'accueil
+    const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerTemplate(photographer);
+        console.log("photographerModel : ", photographerModel);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        console.log("userCardDOM : ", userCardDOM);
+        photographersSection.appendChild(userCardDOM);
+    });
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+async function init() {
+    // Récupère et affiche les datas des photographes
+    const { photographers } = await getPhotographers();
+    console.log("photographers : ", photographers)
+    displayData(photographers);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,62 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
-
+    const { name, id, city, country, tagline, price, portrait } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+        // Création des éléments <article> et <a> : Carte et lien du photographe
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const linkImg = document.createElement("a");
+        // Suppression des espaces dans le nom du photgraphe
+        const nameWoSpace = name.split(' ').join('');
+        linkImg.setAttribute("href", `../scripts/pages/${nameWoSpace}.html`);
+        // Création de l'élément <img> : Portrait du photographe
+        const img = document.createElement("img");
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `Portrait de ${name}`);
+        img.setAttribute("aria-label", `Portrait du photographe ${name}`);
+        // Création de l'élément <h2> : Nom du photographe
+        const h2 = document.createElement("h2");
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        h2.setAttribute("aria-label", `Nom du photographe ${name}`);
+        // Création de l'élément <h3> : Localisation du photographe
+        const h3Location = document.createElement("h3");
+        h3Location.textContent = `${city}, ${country}`;
+        h3Location.setAttribute("aria-label", `Le photographe ${name} est situé à ${city}, ${country}`);
+        // Création de l'élément <h4>  :Slogan du photographe
+        const h4Tagline = document.createElement("h4");
+        h4Tagline.textContent = tagline;
+        h4Tagline.setAttribute("aria-label", `Le slogan du photographe ${name} est ${tagline}`);
+        // Création de l'élément <p> : Tarif du photographe
+        const pPrice = document.createElement("p");
+        pPrice.textContent = `${price}€/jour`;
+        pPrice.setAttribute("aria-label", `Le tarif du photographe ${name} est ${price}€/jour`);
+        // Ajout des éléments à leur parent dans le DOM
+        article.appendChild(linkImg);
+        linkImg.appendChild(img);
+        linkImg.appendChild(h2);
+        article.appendChild(h3Location);
+        article.appendChild(h4Tagline);
+        article.appendChild(pPrice);
+        // Appel de la fonction Listener sur le portrait du photographe
+        listenPhotographer(linkImg, id);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { id, name, picture, getUserCardDOM };
+}
+// Listener sur le portrait du photographe
+function listenPhotographer (linkImg, id) {
+    linkImg.addEventListener("click", async function (){
+        const reponse = await fetch("../../data/photographers.json");
+        const { media } = await reponse.json();
+        // Apple de la fonction de Filtre sur les medias avec l'id du photographe
+        mediaFilter(media, id);
+    })
+}
+// Filtre sur les medias avec l'id du photographe
+function mediaFilter (media, id) {
+    const mediaFiltre = media.filter(function (media){
+        return media.photographerId = id;
+    }) 
+    console.log("mediaFiltre : ", mediaFiltre);
 }
